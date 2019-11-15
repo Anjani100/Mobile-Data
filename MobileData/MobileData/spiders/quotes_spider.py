5b45bbfcdddb3baa5672c354cd562f099ec95a2a
Anjani100 <42367562+Anjani100@users.noreply.github.com>
2019-11-16 00:53:36 +0530

    Update quotes_spider.py

--- MobileData/MobileData/spiders/quotes_spider.py
+++ MobileData/MobileData/spiders/quotes_spider.py
@@ -9,12 +9,12 @@ class QuoteSpider(scrapy.Spider):
 
 	def parse(self, response):
 		items = MobiledataItem()
-		All_div_phones = response.css('._1UoZlX')
-		for phones in All_div_phones:
-			names = phones.css('._3wU53n::text').extract()
-			specs = phones.css('.tVe95H::text').extract()
+		All_div_phones = response.css('._1UoZlX') ## Extracting the whole division tag
+		for phones in All_div_phones: ## Iterating through all the phone's information
+			names = phones.css('._3wU53n::text').extract() ## Extracting the mobile phone's name
+			specs = phones.css('.tVe95H::text').extract() ## Extracting the specifications of the phones. Since different companies have different pattern for showing the specification, it was not possible to categorise the data into memory, processor, etc.
 			price = phones.css('._2rQ-NK::text').extract()
-			name1 = names[0].split('(')
+			name1 = names[0].split('(') ## Splitting the name and the color of the mobile phones
 			name = name1[0]
 			
 			items['Name'] = name
